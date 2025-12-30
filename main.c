f5644af8a0a1cb32f00033eb48054d9454e3729f
Samirborges <alessandro.samir@souunit.com.br>
2025-12-30 17:56:38 -0300

    Add TOKEN_PLUS supprt and fix number token classification
    - Added TOKEN_PLUS to enum and lexer handling for '+' operator
    
    - Updated token_tupe_to_string() to include TOKEN_PLUS
    
    - Fixed number recoginition to use TOKEN_NUMBER instead of TOKEN_INT
    
    - Improved token priting to show token names insead of num values

--- main.c
+++ main.c
@@ -5,6 +5,13 @@
 typedef enum
 {
     TOKEN_INT,
+    TOKEN_PLUS,
+    TOKEN_IF,
+    TOKEN_LPAREN,
+    TOKEN_RPAREN,
+    TOKEN_LESS,
+    TOKEN_LBRACE,
+    TOKEN_RBRACE,
     TOKEN_IDENTIFIER,
     TOKEN_NUMBER,
     TOKEN_ASSIGN,
@@ -22,19 +29,20 @@ typedef struct
 Token get_next_token();
 char peak();
 char advanced();
+const char *token_type_to_string(TokenType type);
 
 const char *source;
 int pos = 0;
 
 int main()
 {
-    source = "int x = 42;";
+    source = "if (x < 10) {\nx = x + 1;\n}";
     Token t;
 
     do
     {
         t = get_next_token();
-        printf("Token: %d, Lexeme: '%s'\n", t.type, t.lexeme);
+        printf("Token: %s, Lexeme: '%s'\n", token_type_to_string(t.type), t.lexeme);
     } while (t.type != TOKEN_EOF);
 
     return 0;
@@ -85,12 +93,64 @@ Token get_next_token()
         {
             token.type = TOKEN_INT;
         }
+        else if (strcmp(token.lexeme, "if") == 0)
+        {
+            token.type = TOKEN_IF;
+        }
         else
         {
             token.type = TOKEN_IDENTIFIER;
         }
 
         return token;
+    }
+
+    // Identificar qualquer caractere imprimível que não seja alfanumérico ou espaço
+    if (ispunct(c))
+    {
+        // Símbolos compostos
+        if (c == '<')
+        {
+            token.lexeme[0] = advanced();
+            token.lexeme[1] = '\0';
+            token.type = TOKEN_LESS;
+            return token;
+        }
+        else if (c == '+')
+        {
+            token.lexeme[0] = advanced();
+            token.lexeme[1] = '\0';
+            token.type = TOKEN_PLUS;
+            return token;
+        }
+        else if (c == '(')
+        {
+            token.lexeme[0] = advanced();
+            token.lexeme[1] = '\0';
+            token.type = TOKEN_LPAREN;
+            return token;
+        }
+        else if (c == ')')
+        {
+            token.lexeme[0] = advanced();
+            token.lexeme[1] = '\0';
+            token.type = TOKEN_RPAREN;
+            return token;
+        }
+        else if (c == '{')
+        {
+            token.lexeme[0] = advanced();
+            token.lexeme[1] = '\0';
+            token.type = TOKEN_LBRACE;
+            return token;
+        }
+        else if (c == '}')
+        {
+            token.lexeme[0] = advanced();
+            token.lexeme[1] = '\0';
+            token.type = TOKEN_RBRACE;
+            return token;
+        }
     }
 
     // Números
@@ -101,7 +161,7 @@ Token get_next_token()
             token.lexeme[i++] = advanced();
         }
         token.lexeme[i] = '\0';
-        token.type = TOKEN_INT;
+        token.type = TOKEN_NUMBER;
         return token;
     }
 
@@ -133,4 +193,41 @@ char peak()
 char advanced()
 {
     return source[pos++];
+}
+
+const char *token_type_to_string(TokenType type)
+{
+    switch (type)
+    {
+    case TOKEN_INT:
+        return "TOKEN_INT";
+    case TOKEN_IF:
+        return "TOKEN_IF";
+    case TOKEN_LPAREN:
+        return "TOKEN_LPAREN";
+    case TOKEN_RPAREN:
+        return "TOKEN_RPAREN";
+    case TOKEN_LESS:
+        return "TOKEN_LESS";
+    case TOKEN_LBRACE:
+        return "TOKEN_LBRACE";
+    case TOKEN_RBRACE:
+        return "TOKEN_RBRACE";
+    case TOKEN_IDENTIFIER:
+        return "TOKEN_IDENTIFIER";
+    case TOKEN_NUMBER:
+        return "TOKEN_NUMBER";
+    case TOKEN_ASSIGN:
+        return "TOKEN_ASSIGN";
+    case TOKEN_SEMICOLON:
+        return "TOKEN_SEMICOLON";
+    case TOKEN_EOF:
+        return "TOKEN_EOF";
+    case TOKEN_ERROR:
+        return "TOKEN_ERROR";
+    case TOKEN_PLUS:
+        return "TOKEN_PLUS";
+    default:
+        return "UNKNOWN_TOKEN";
+    }
 }
